333bd9662831b3c8f3afe229151ad900153b3680
Shivambhart1 <shivama.21.becs@acharya.ac.in>
2025-01-22 12:38:57 +0530

    checking prod api

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ const allowedOrigins = [
 ];
 app.use(cors({
   origin: allowedOrigins,
-  // credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(bodyParser.json());
 
